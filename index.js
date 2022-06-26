bbf4ae8c7992a97a9cb7e7aea551c50e412d2e5a
roy-eugene049 <roy.eugenes000@gmail.com>
2022-06-26 21:49:40 +0300

    Game Interface

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ let secondCard = 11;
 let sum = firstCard + secondCard;
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂")
-} else if (sum === 21) {
-    console.log("Woohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! 😭")
-    isAlive = false
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Woohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    console.log(message)
 }
 
-console.log(isAlive)
+
+
