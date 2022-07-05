c98d850f9f348be337056d275efecd748919d257
roy-eugene049 <roy.eugenes000@gmail.com>
2022-07-05 20:44:53 +0300

    Fixing Start Game Function

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 
 let messageEl = document.getElementById("message-el")
@@ -23,6 +20,11 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard;
     renderGame()
 }
 
