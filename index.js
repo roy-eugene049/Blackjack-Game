d6e4cfb901063d6b131d2fbeedb80a4944dcfd8f
roy-eugene049 <roy.eugenes000@gmail.com>
2022-07-07 19:04:14 +0300

    Adding Objects

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
 
+let player = {
+    name: "Roy",
+    chips: 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1
     if (randomNumber > 10) {
